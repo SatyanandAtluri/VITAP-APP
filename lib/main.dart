c58d52d375b8cecb335658a7f8ac620d66f6fa2b
SatyanandAtluri <satyanand7789@gmail.com>
2023-03-13 01:22:29 +0530

    Added Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:vitap_redesign/screens%20/Splash_Screen.dart';
 
-void main() {
+void main() async {
+  await ScreenUtil.ensureScreenSize();
   runApp(const MyApp());
 }
 
@@ -11,10 +13,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'VITAP APP',
-      debugShowCheckedModeBanner: false,
-      home: Splash_Screen(),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp(
+              title: 'VITAP APP',
+              theme: ThemeData(
+                useMaterial3: true,
+                primarySwatch: Colors.blue,
+              ),
+              debugShowCheckedModeBanner: false,
+              home: Splash_Screen());
+        });
   }
 }
--- lib/screens /Splash_Screen.dart
+++ lib/screens /Splash_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'background.dart';
 
@@ -17,8 +18,20 @@ class _Splash_ScreenState extends State<Splash_Screen> {
       body: Scaffold_Background(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Container()],
+          children: [
+            Center(
+              child: Container(
+                height: 50.h,
+                width: 150.w,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/VITAP.jpg"),
+                      fit: BoxFit.fill),
+                  color: Color(0xff181A20),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
