b6923c6d575260f4634cdc04053eda6a874a6a7e
SatyanandAtluri <satyanand7789@gmail.com>
2023-03-13 22:01:32 +0530

    minor changes
    + added background color

--- lib/pages/LoginPage/onbording_page.dart
+++ lib/pages/LoginPage/onbording_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:vitap_redesign/pages/SplashPage/background.dart';
 
 class OnbordingPage extends StatelessWidget {
   const OnbordingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xff181A20),
+      body: Scaffold_Background(
+        child: Column(),
+      ),
+    );
   }
 }
