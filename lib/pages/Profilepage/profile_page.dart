edd6cde5f7d0f5b0b5e3cbe573ccaf72fcaf0b53
SatyanandAtluri <satyanand7789@gmail.com>
2023-03-26 14:31:42 +0530

    changes in profile page

--- lib/pages/Profilepage/profile_page.dart
+++ lib/pages/Profilepage/profile_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:vitap_redesign/Customs/custom_tap.dart';
 
+import '../LoginPage/login_page.dart';
+
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
@@ -159,6 +161,11 @@ class ProfilePage extends StatelessWidget {
                 children: [
                   CircleAvatar(
                     radius: 30.r,
+                    backgroundColor: Color(0xffffffff),
+                    child: Image.asset(
+                      'assets/images/photo.png',
+                      fit: BoxFit.fill,
+                    ),
                   ),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -214,6 +221,279 @@ class ProfilePage extends StatelessWidget {
                   )
                 ],
               ),
+            ),
+            CustomTap(
+              onTap: () {},
+              child: Padding(
+                padding: EdgeInsets.all(10.h),
+                child: ListTile(
+                  title: Text(
+                    'Profile',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16.sp,
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Check your personal information',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.sp,
+                      color: Color(0xffBDB9B9),
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Color(0xff000000),
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+              color: Color(0xffBDB9B9),
+            ),
+            CustomTap(
+              onTap: () {},
+              child: Padding(
+                padding: EdgeInsets.all(10.h),
+                child: ListTile(
+                  title: Text(
+                    'Wifi-Connect',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16.sp,
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Connect to the Wifi In-Campus',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.sp,
+                      color: Color(0xffBDB9B9),
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Color(0xff000000),
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+              color: Color(0xffBDB9B9),
+            ),
+            CustomTap(
+              onTap: () {},
+              child: Padding(
+                padding: EdgeInsets.all(10.h),
+                child: ListTile(
+                  title: Text(
+                    'Advertise',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16.sp,
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Promotion related queries',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.sp,
+                      color: Color(0xffBDB9B9),
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Color(0xff000000),
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+              color: Color(0xffBDB9B9),
+            ),
+            CustomTap(
+              onTap: () {},
+              child: Padding(
+                padding: EdgeInsets.all(10.h),
+                child: ListTile(
+                  title: Text(
+                    'Notification',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16.sp,
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Control your Notifications ',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.sp,
+                      color: Color(0xffBDB9B9),
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Color(0xff000000),
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+              color: Color(0xffBDB9B9),
+            ),
+            CustomTap(
+              onTap: () {},
+              child: Padding(
+                padding: EdgeInsets.all(10.h),
+                child: ListTile(
+                  title: Text(
+                    'Help Centre',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16.sp,
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Get Help if you have any issues',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.sp,
+                      color: Color(0xffBDB9B9),
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Color(0xff000000),
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+              color: Color(0xffBDB9B9),
+            ),
+            CustomTap(
+              onTap: () {},
+              child: Padding(
+                padding: EdgeInsets.all(10.h),
+                child: ListTile(
+                  title: Text(
+                    'Tell a Friend',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16.sp,
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Share the Application Now',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.sp,
+                      color: Color(0xffBDB9B9),
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Color(0xff000000),
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+              color: Color(0xffBDB9B9),
+            ),
+            CustomTap(
+              onTap: () {},
+              child: Padding(
+                padding: EdgeInsets.all(10.h),
+                child: ListTile(
+                  title: Text(
+                    'User Agreement',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16.sp,
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                  subtitle: Text(
+                    'user agreement for our services',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.sp,
+                      color: Color(0xffBDB9B9),
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Color(0xff000000),
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+              color: Color(0xffBDB9B9),
+            ),
+            CustomTap(
+              onTap: () {},
+              child: Padding(
+                padding: EdgeInsets.all(10.h),
+                child: ListTile(
+                  title: Text(
+                    'Privacy policy',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16.sp,
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Our End-To-End Privacy Policies',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.sp,
+                      color: Color(0xffBDB9B9),
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Color(0xff000000),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30.h,
+            ),
+            CustomTap(
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => LoginPage()));
+              },
+              child: Text(
+                'Log Out',
+                style: GoogleFonts.poppins(
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xff000000),
+                ),
+              ),
+            ),
+            Text(
+              'Version 0.0.1',
+              style: GoogleFonts.poppins(
+                fontSize: 14.sp,
+                fontWeight: FontWeight.w400,
+                color: Color(0xff000000),
+              ),
+            ),
+            SizedBox(
+              height: 10.h,
             ),
           ],
         ),
